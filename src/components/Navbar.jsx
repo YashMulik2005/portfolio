188a76e1e24a09beb59b195f0b7bcf098d560f72
Your Name <you@example.com>
2023-06-15 17:03:59 +0530

    animation done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import themehook from './ThemeContext'
 import { GiHamburgerMenu } from 'react-icons/gi'
+import { ImCancelCircle } from 'react-icons/im'
 
 function Navbar() {
     const { theme, changetheme } = themehook()
@@ -27,16 +28,17 @@ function Navbar() {
                 <h1 className={`text-xl ${theme == 'dark' ? ' text-green-500' : ' text-green-700'} font-bold`}>Portfolio</h1>
                 {/* <button onClick={handletoggle} className='sm:hidden'>click</button> */}
                 <section onClick={handletoggle} className='sm:hidden'>
-                    <GiHamburgerMenu size={23} />
+                    {height ? <ImCancelCircle size={23} /> : <GiHamburgerMenu size={23} />}
                 </section>
-                <button onClick={handletheme} className=''>theme</button>
+
+                {/* <button onClick={handletheme} className=''>theme</button> */}
             </nav>
             <div >
-                <ul className={`sm:flex justify-center items-center shadow-md m-1 ${height ? "h-28" : "h-0 overflow-hidden"} sm:h-auto`}>
-                    <li className='m-1 font-semibold'><a href='#about'>About</a></li>
-                    <li className='m-1 font-semibold'><a href='#contact'>Contact</a></li>
-                    <li className='m-1 font-semibold'><a href='#project'>Projects</a></li>
-                    <li className='m-1 font-semibold'><a href='#skills'>Skills</a></li>
+                <ul className={`sm:flex justify-center items-center shadow-md m-1 ${height ? "h-28" : "h-0 overflow-hidden"} sm:h-auto transition-all ease-linear duration-300 `}>
+                    <li className='m-1 font-semibold '><a href='#about'>About</a></li>
+                    <li className='m-1 font-semibold '><a href='#contact'>Contact</a></li>
+                    <li className='m-1 font-semibold '><a href='#project'>Projects</a></li>
+                    <li className='m-1 font-semibold '><a href='#skills'>Skills</a></li>
                 </ul>
             </div>
         </div>
